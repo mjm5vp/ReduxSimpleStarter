4ce379d15ff1c888d54483abf43938fe5eca5622
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-09-11 23:45:30 -0400

    added video detail component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = "AIzaSyDCrlDczVSios59ysyIklqOqsm8RT_IVKo"
 
+
+
 //Component to create some HTML
-const App = () => {
-  return <div>Hi</div>
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos
+      })
+    })
+  }
+
+  render(){
+    return (<div>
+      <SearchBar />
+      <VideoList videos={this.state.videos}/>
+    </div>
+    )
+  }
 }
 
 //Take the component's generated HTML and put it in the home page (in the DOM)
